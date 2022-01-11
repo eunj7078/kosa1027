943919cb6291e4517f1aea30076c4f8a08346365
user <user@DESKTOP-4NMVE1B>
2022-01-11 17:27:02 +0900

    20220111 dependency1

---
+++ dependencyTest1/src/main/service/MemberListPrinter.java
@@ -0,0 +1,17 @@
+package main.service;
+
+import java.util.Collection;
+
+import main.DTO.MemberDTO;
+import main.DTO.MemberDao;
+
+public class MemberListPrinter {
+	MemberDao memberDao = new MemberDao();
+	MemberPrinter print = new MemberPrinter();
+	public void printAll() {
+		Collection<MemberDTO> lists = memberDao.selectAll();
+		for(MemberDTO dto : lists) {
+			print.print(dto);
+		}
+	}
+}
---
+++ dependencyTest1/src/main/service/MemberRegisterService.java
@@ -0,0 +1,26 @@
+package main.service;
+
+import java.util.Date;
+
+import main.DTO.MemberDTO;
+import main.DTO.MemberDao;
+import main.DTO.RegisterRequest;
+
+public class MemberRegisterService {
+	//의존객체(dependency object)
+	MemberDao memberDao = new MemberDao();
+	
+	public void execute(RegisterRequest request) {
+		MemberDTO dto = memberDao.selectByEmail(request.getEmail());
+		if(dto == null) {
+			dto.setEmail(request.getEmail());
+			dto.setName(request.getName());
+			dto.setPassword(request.getPassword());
+			dto.setRegisterDate(new Date());
+			memberDao.insert(dto);
+			System.out.println("사용자 등록이 완료되었습니다.");
+		}else {
+			System.out.println("중복 사용자입니다.");
+		}		
+	}
+}
