8cb2bef68c7014c67b3826e3d4f9e5bcd8ee4da7
user <user@DESKTOP-4NMVE1B>
2022-01-11 17:28:09 +0900

    20220111 dependency4

---
+++ dependencyTest4/src/main/java/spring/dependencyTest4/service/MemberInfoPrinter.java
@@ -0,0 +1,20 @@
+package spring.dependencyTest4.service;
+
+import spring.dependencyTest4.DTO.MemberDTO;
+import spring.dependencyTest4.DTO.MemberDao;
+
+public class MemberInfoPrinter {
+	MemberDao memberDao;
+	MemberPrinter print = new MemberPrinter();
+	public MemberInfoPrinter(MemberDao memberDao) {
+		this.memberDao = memberDao;
+	}
+	public void execute(String email) {
+		MemberDTO dto = memberDao.selectByEmail(email); //의존객체 아님
+		if(dto == null) {
+			System.out.println("데이터 없음 \n");
+			return;
+		}
+		print.print(dto);
+	}
+}
